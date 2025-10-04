24e9a6d7204a4caa4b11d0f8500738474dae4b65
bhtomson <ben@bhtfilms.com>
2025-10-05 00:58:24 +0200

    chore: enable proxy and url for Render

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,8 +1,18 @@
 module.exports = ({ env }) => ({
-  proxy: true,
-  url: env('PUBLIC_URL', env('RENDER_EXTERNAL_URL', '')),
+  proxy: true, // trust Render's reverse proxy
   host: '0.0.0.0',
   port: env.int('PORT', 1337),
-  app: { keys: env.array('APP_KEYS') },    // <- IMPORTANT
-  admin: { auth: { secret: env('ADMIN_JWT_SECRET') } },
+
+  // ensures correct absolute URLs for admin and API
+  url: env('PUBLIC_URL', env('RENDER_EXTERNAL_URL', '')),
+
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
+    },
+  },
 });
