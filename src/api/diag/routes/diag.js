f9a65b1ea06869ae7ee1c590c5397bde5e6e3e6f
bhtomson <ben@bhtfilms.com>
2025-10-05 17:05:35 +0200

    chore: add __diag runtime inspector

--- src/api/diag/routes/diag.js
+++ src/api/diag/routes/diag.js
@@ -1,5 +1,3 @@
 module.exports = {
-  routes: [
-    { method: 'GET', path: '/__diag', handler: 'diag.index', config: { auth: false } },
-  ],
+  routes: [{ method: 'GET', path: '/__diag', handler: 'diag.index', config: { auth: false } }],
 };
