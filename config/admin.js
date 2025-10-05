7f7629ab5eb2d74de1928d7d03d79ae5f2861d36
bhtomson <ben@bhtfilms.com>
2025-10-05 16:33:40 +0200

    chore: align render buildCommand with package manager

--- config/admin.js
+++ config/admin.js
@@ -2,18 +2,18 @@ module.exports = ({ env }) => ({
   apiToken: {
     salt: env('API_TOKEN_SALT'),
     secrets: {
-      encryptionKey: env('ADMIN_ENCRYPTION_KEY'), // fixes "Encryption key is missing"
+      encryptionKey: env('ADMIN_ENCRYPTION_KEY'), // add this env var
     },
   },
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
     sessions: {
-      accessTokenLifespan: env.int('ADMIN_ACCESS_TOKEN_LIFESPAN', 1800),           // 30m
-      maxRefreshTokenLifespan: env.int('ADMIN_MAX_REFRESH_TOKEN_LIFESPAN', 2592000), // 30d
+      accessTokenLifespan: env.int('ADMIN_ACCESS_TOKEN_LIFESPAN', 1800),              // 30m
+      maxRefreshTokenLifespan: env.int('ADMIN_MAX_REFRESH_TOKEN_LIFESPAN', 2592000),  // 30d
+      maxSessionLifespan: env.int('ADMIN_MAX_SESSION_LIFESPAN', 2592000),             // 30d
       idleRefreshTokenLifespan: env.int('ADMIN_IDLE_REFRESH_TOKEN_LIFESPAN', 604800), // 7d
     },
   },
-  transfer: {
-    token: { salt: env('TRANSFER_TOKEN_SALT') },
-  },
+  transfer: { token: { salt: env('TRANSFER_TOKEN_SALT') } },
 });
+
