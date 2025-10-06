1c045b274df3e90064d02fa43b731d445878505a
bhtomson <ben@bhtfilms.com>
2025-10-06 16:25:11 +0200

    feat: add Cloudinary upload provider and CSP config for Strapi v5

---
+++ config/plugins.ts
@@ -0,0 +1,21 @@
+import { type Core } from '@strapi/strapi';
+
+const config: Core.PluginConfig = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
+
+export default config;
